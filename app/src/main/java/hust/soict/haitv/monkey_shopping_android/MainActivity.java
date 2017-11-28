8e1b0b66b92ce9bb7c86e8a6085657caee921dd0
HÃ??Ã?Â¡Ã??Ã?ÂºÃ??Ã?Â£i ThÃ???Ã??Ã?Â¢n VÃ????n <thanhaibkk58@gmail.com>
2017-11-28 14:07:13 +0700

    Init Project
    Signed-off-by: Hải Thân Văn <thanhaibkk58@gmail.com>

---
+++ app/src/main/java/hust/soict/haitv/monkey_shopping_android/MainActivity.java
@@ -0,0 +1,13 @@
+package hust.soict.haitv.monkey_shopping_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
